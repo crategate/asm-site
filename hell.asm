94890f2455d0b7dcddfc9774ef042a046583a8f8
michael pratt <sevensidedsilver@gmail.com>
2025-08-02 15:00:29 -0400

    write

--- hell.asm
+++ hell.asm
@@ -1,3 +1,22 @@
 format ELF64 executable 3
 
+segment readable executable
 
+entry main
+
+main:
+  lea   rdi, [msg]      ; address of msg to rdi
+  mov   rax, 16         ;put length of msg to rax
+  mov   rdx, rax        ;rax to rdx
+  mov   rsi, rdi
+  mov   rdi, 1          ; std out
+  mov   rax, 1          ; sys_write
+  syscall
+  xor   rdi, rdi      ; exit code 0
+  mov   rax, 60       ; sys_exit
+
+
+segment readable writeable
+
+
+msg   db 'hell u made it' , 10, 0

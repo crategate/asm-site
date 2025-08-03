09a429aa1261a8fe83e2e75088994227ceaa3650
michael pratt <sevensidedsilver@gmail.com>
2025-08-03 14:33:51 -0400

    add second message then exit

--- hell.asm
+++ hell.asm
@@ -12,11 +12,25 @@ main:
   mov   rdi, 1          ; std out
   mov   rax, 1          ; sys_write
   syscall
+
+  lea   rdi, [msg2]      ; address of msg to rdi
+  mov   rax, 19         ;put length of msg to rax
+  mov   rdx, rax        ;rax to rdx
+  mov   rsi, rdi
+  mov   rdi, 1          ; std out
+  mov   rax, 1          ; sys_write
+  syscall
+
+
+
+
   xor   rdi, rdi      ; exit code 0
   mov   rax, 60       ; sys_exit
 
+  syscall
 
 segment readable writeable
 
 
 msg   db 'hell u made it' , 10, 0
+msg2   db 'hell000 u made it' , 10, 0
